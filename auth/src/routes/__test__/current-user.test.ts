d359c5a8db2ac7b05778bb82533ae3a2af6c3c06
leoptomg <leopto.mg@gmail.com>
2022-01-04 18:33:53 -0300

    Begin client-nextjs

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("signup ok", async () => {
+it("auth user ok", async () => {
   const cookie = await signin()
 
   const res = await request(app)
@@ -12,4 +12,13 @@ it("signup ok", async () => {
 
   // console.log(res.body)
   expect(res.body.currentUser.email).toEqual('ana@gmail.com')
+});
+
+it("auth user fail", async () => {
+  const res = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200)
+
+  expect(res.body.currentUser).toEqual(null)
 });
