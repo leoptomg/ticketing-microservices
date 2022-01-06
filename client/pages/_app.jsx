eb6ec747bfcae3337602f1b3c4634624b6912f73
leoptomg <leopto.mg@gmail.com>
2022-01-06 07:33:12 -0300

    Auth service - END

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build-client";
+import Header from "../components/header";
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
+
   return (
     <div>
-      <h1>{currentUser && currentUser.email}</h1>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </div>
   );
@@ -13,17 +15,17 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get("/api/users/currentuser");
-  
-  let pageProps = {}
 
-  if(appContext.Component.getInitialProps){
-      pageProps = await appContext.Component.getInitialProps(appContext.ctx)
-  } 
-//   console.log(data);
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  //   console.log(data);
   return {
     pageProps,
-    ...data
-  }
+    ...data,
+  };
 };
 
 export default AppComponent;
